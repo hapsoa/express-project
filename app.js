51aa62d4fc7321ba61960bd920f56a15361aa486
SharingWorld <hapsoa@gmail.com>
2018-06-19 10:39:47 +0900

    06 19

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var sassMiddleware = require('node-sass-middleware');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kakaotalkRouter = require('./routes/kakaotalk');
+var calculatorRouter = require('./routes/calculator');
 
 var app = express();
 
@@ -29,7 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/kakaotalk', kakaotalkRouter)
+app.use('/kakaotalk', kakaotalkRouter);
+app.use('/calculator', calculatorRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
