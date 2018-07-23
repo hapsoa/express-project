e01824913429af2b8f6d1a45f58875085109e797
SharingWorld <hapsoa@gmail.com>
2018-07-23 18:52:44 +0900

    update

--- public/javascripts/main-page/data.js
+++ public/javascripts/main-page/data.js
@@ -1,14 +1,70 @@
 const dataManager = new function() {
-    const data = [];
+    const dataArray = [];
 
-    const calculatorData = {
-        name: 'calculator',
+    dataArray.push({
+        href: '/calculator',
         cellSize: 'small'
-    };
+    });
 
-    data.push(calculatorData);
+    dataArray.push({
+        href: '/kakaotalk/login',
+        cellSize: 'small'
+    });
 
-    
+    dataArray.push({
+        href: '/fractal/education',
+        cellSize: 'big'
+    });
+
+    dataArray.push({
+        href: '/textFinder',
+        cellSize: 'small'
+    });
+
+    dataArray.push({
+        href: '/ajou',
+        cellSize: 'long'
+    });
+
+    dataArray.push({
+        href: '/firebase',
+        cellSize: 'small'
+    });
+
+    dataArray.push({
+        href: '/modal',
+        cellSize: 'big'
+    });
+
+    dataArray.push({
+        href: '/grid-1',
+        cellSize: 'small'
+    });
+
+    dataArray.push({
+        href: '/card-4',
+        cellSize: 'small'
+    });
+
+    dataArray.push({
+        href: '/table',
+        cellSize: 'big'
+    });
+
+    const $root = $('.grid');
+
+    dataArray.forEach((element) => {
+        const template = `
+    <a class="grid-item ${element.cellSize}" href="${element.href}">
+        <div class="curtain"></div>
+    </a>
+    `;
+
+        $root.append(template);
+    });
+
+
+
 
 };
 
